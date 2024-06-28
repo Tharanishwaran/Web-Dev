c33ac9a1b07d4fd62c1f53a4a711657bf198ce9f
Tharanishwara <tharanishwaran.ta@gmail.com>
2024-06-28 22:27:08 +0530

    props added for title and subtitle

---
+++ Learn-Reactjs/src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Card({children}) {
+  return (
+    <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+      
+   
+
+
+    </div>
+  )
+}
+
+export default Card
