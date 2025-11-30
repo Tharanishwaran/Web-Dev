8bcb0899e0065ebb5f11ac869c13ba651faa36de
Tharanishwaran <tharanishwaran.ta@gmail.com>
2025-11-30 22:37:04 +0530

    usereducer added

---
+++ JS/Asynchronous/settimeout/main.js
@@ -0,0 +1,15 @@
+console.log("hello..")
+function sync(){
+
+    console.log("step 1")
+    console.log("step 2")
+    console.log("step 3")
+
+}
+sync()
+
+let a = 100
+let b = 20
+let c = a+b
+
+setTimeout(()=>console.log(""))
